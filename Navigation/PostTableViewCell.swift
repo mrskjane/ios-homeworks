5885253dbbdd0c39b9d9ffb33fbd0ac8b8850519
Panfilova Evgeniya <mrs.kjane@gmail.com>
2025-12-15 11:40:19 +0300

    created 3 posts

---
+++ Navigation/PostTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostTableViewCell.swift
+//  Navigation
+//
+//  Created by Евгения Панфилова on 13.12.2025.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
