82ab7a2f0391df554ac4b89e6941dbc2be3c4cb6
Panfilova Evgeniya <mrs.kjane@gmail.com>
2025-12-20 07:22:58 +0300

    added photos

--- Navigation/PhotosTableViewCell.swift
+++ Navigation/PhotosTableViewCell.swift
@@ -1,23 +1,51 @@
-//
-//  PhotosTableViewCell.swift
-//  Navigation
-//
-//  Created by Евгения Панфилова on 19.12.2025.
-//
 
 import UIKit
 
 class PhotosTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    private let photosLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Photos"
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.textColor = .label
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let arrowImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "arrow.right")
+        imageView.tintColor = .black
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let photosStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually // делит место поровну
+        stackView.spacing = 8 // расстояние между фото
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupLayout()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
+    private func setupLayout() {
+        contentView.addSubview(photosLabel)
+        contentView.addSubview(arrowImageView)
+        contentView.addSubview(photosStackView)
+        
+        NSLayoutConstraint.activate([
+            photosLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+    }
+    
 }
