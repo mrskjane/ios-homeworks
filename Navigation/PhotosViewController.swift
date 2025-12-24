9b3a5e3c1ee10617f1ce052359da615399af3e83
Panfilova Evgeniya <mrs.kjane@gmail.com>
2025-12-24 19:51:17 +0300

    added all photos

--- Navigation/PhotosViewController.swift
+++ Navigation/PhotosViewController.swift
@@ -3,7 +3,7 @@ import UIKit
 
 class PhotosViewController: UIViewController {
     
-    private let photos: [String] = ["Image_1", "Image_2", "Image_3", "Image_4", "Image_5", "Image_6", "Image_7", "Image_8", "Image_9", "Image_10", "Image_1", "Image_2", "Image_3", "Image_4", "Image_5", "Image_6", "Image_7", "Image_8", "Image_9", "Image_10"]
+    private let photos: [String] = ["Image_1", "Image_2", "Image_3", "Image_4", "Image_5", "Image_6", "Image_7", "Image_8", "Image_9", "Image_10", "Image_11", "Image_12", "Image_13", "Image_14", "Image_15", "Image_16", "Image_17", "Image_18", "Image_19", "Image_20"]
     
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
